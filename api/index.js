2e3b2ce686d92e7a60cf4dfef8c43bf9ea1f8b10
Indra Palijama <indrapalijama@gmail.com>
2023-07-25 13:42:41 +0700

    testing deploy 2

--- api/index.js
+++ api/index.js
@@ -71,3 +71,8 @@ client.on("message", async (message) => {
     message.reply("pong");
   }
 });
+
+module.exports = (req, res) => {
+  const name = req.query.name || "stranger";
+  res.status(200).send(`Hello, ${name}!`);
+};
